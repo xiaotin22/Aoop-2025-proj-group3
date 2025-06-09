59a07073f1631843e7ad7cb76ff08f34bb525e93
xiaotin22 <terry941020@gmail.com>
2025-06-09 20:34:54 +0800

    Initialize pygame mixer for audio support

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import os
 from UI.start_select import CharacterSelectScene
 
 pygame.init()
+pygame.mixer.init()
+
 # Define screen dimensions
 SCREEN_HEIGHT = 800
 SCREEN_WIDTH = 1200
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('Game Start')
+
+
 
 scene = CharacterSelectScene(screen)
 selected_character = scene.run()
