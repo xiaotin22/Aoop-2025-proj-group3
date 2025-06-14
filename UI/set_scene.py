e42dc2dbe0e52fcbc644abe5117981e59d445530
HSIN-HUI YANG <yxh.ee13@nycu.edu.tw>
2025-06-14 20:26:56 +0800

    Adjust button positions in SetScene for improved layout

--- UI/set_scene.py
+++ UI/set_scene.py
@@ -20,8 +20,8 @@ class SetScene(BaseScene):
         self.back_hover = False
 
         # ✅ 設定按鈕（可自由調整位置與大小）
-        self.button1 = ImageButton("resource/image/button.png", (150, 140), size=(650, 550))
-        self.button2 = ImageButton("resource/image/button.png", (150, 350), size=(650, 550))
+        self.button1 = ImageButton("resource/image/button.png", (200, 110), size=(650, 550))
+        self.button2 = ImageButton("resource/image/button.png", (200, 320), size=(650, 550))
 
     def run(self):
         while self.running:
