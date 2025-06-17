49a05cef727af5e825c18c5a4042f2558ca5a43b
HSIN-HUI YANG <yxh.ee13@nycu.edu.tw>
2025-06-17 12:12:03 +0800

    Refactor SetScene to adjust button positioning for improved layout

--- UI/set_scene.py
+++ UI/set_scene.py
@@ -18,8 +18,8 @@ class SetScene(BaseScene):
         self.back_hover = False
 
         # 👇 兩個 hover 放大圖片按鈕
-        self.button1 = ImageButton("resource/image/button.png", (300, 55), size=(600, 450))
-        self.button2 = ImageButton("resource/image/button.png", (300, 215), size=(600, 450))
+        self.button1 = ImageButton("resource/image/button.png", (300, 95), size=(600, 450))
+        self.button2 = ImageButton("resource/image/button.png", (300, 295), size=(600, 450))
 
         # 字體（週數）
         self.week_font = pygame.font.Font("resource/font/ChenYuluoyan-Thin-Monospaced.ttf", 42)
